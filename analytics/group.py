8a0e25a70780eb3621de906a2eb6027523a8f3ed
Ravi Rajamani <ravibeta@hotmail.com>
2025-05-11 20:32:56 -0600

    update embeddings

--- analytics/group.py
+++ analytics/group.py
@@ -1,3 +1,9 @@
+import sift
+import cv2
+import json
+import math
+import multiprocessing
+
 def stable_groups(keypoints, groups, threshold):
     for kp in keypoints:
         matched = false
@@ -22,3 +28,77 @@ def global_groups(stable_groups,global_groups, threshold):
                break
         if matched == false:
             global_groups.add(create_global_group(stable_group))
+
+def get_recent_pixel(bboxes: list):
+    """
+    Finds the bounding box closest to the center of all given bounding boxes.
+
+    :param bboxes: List of tuples containing bounding box coordinates and a float value.
+                   Each tuple is ((x_min, y_min, x_max, y_max), value).
+    :return: Tuple representing the bounding box closest to the center of all bounding boxes.
+    """
+    if not bboxes:
+        return None
+
+    # Compute the overall bounding box center
+    all_x_mins = [bbox[0][0] for bbox in bboxes]
+    all_y_mins = [bbox[0][1] for bbox in bboxes]
+    all_x_maxs = [bbox[0][2] for bbox in bboxes]
+    all_y_maxs = [bbox[0][3] for bbox in bboxes]
+
+    center_x = (min(all_x_mins) + max(all_x_maxs)) / 2
+    center_y = (min(all_y_mins) + max(all_y_maxs)) / 2
+
+    def bbox_distance(bbox):
+        x_min, y_min, x_max, y_max = bbox[0]
+        bbox_center_x = (x_min + x_max) / 2
+        bbox_center_y = (y_min + y_max) / 2
+        return ((bbox_center_x - center_x) ** 2 + (bbox_center_y - center_y) ** 2) ** 0.5
+
+    return min(bboxes, key=bbox_distance)
+
+def get_mean_feature(bboxes: list):
+    """
+    Computes the mean of all values associated with bounding boxes.
+
+    :param bboxes: List of tuples containing bounding box coordinates and a float value.
+                   Each tuple is ((x_min, y_min, x_max, y_max), value).
+    :return: Mean of the float values.
+    """
+    if not bboxes:
+        return 0.0
+
+    return sum(value for _, value in bboxes) / len(bboxes)
+
+
+
+LK_PARAMETERS = dict(winSize=(21, 21), maxLevel=2, criteria=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 30, 0.01))
+
+def homography_from_flow(prev_homography, prev_gray, cur_gray):
+	positions = []
+	for i in range(0, prev_gray.shape[0]-50, 50):
+		for j in range(0, prev_gray.shape[1]-50, 50):
+			positions.append((i, j))
+	positions_np = numpy.array(positions, dtype='float32').reshape(-1, 1, 2)
+
+	def flip_pos(positions):
+		return numpy.stack([positions[:, :, 1], positions[:, :, 0]], axis=2)
+
+	next_positions, st, err = cv2.calcOpticalFlowPyrLK(prev_gray, cur_gray, flip_pos(positions_np), None, **LK_PARAMETERS)
+	if next_positions is None:
+		return None
+
+	next_positions = flip_pos(next_positions)
+	differences = next_positions[:, 0, :] - positions_np[:, 0, :]
+	differences_okay = differences[numpy.where(st[:, 0] == 1)]
+	median = [numpy.median(differences_okay[:, 0]), numpy.median(differences_okay[:, 1])]
+	good = (numpy.square(differences[:, 0] - median[0]) + numpy.square(differences[:, 1] - median[1])) < 16
+
+	if float(numpy.count_nonzero(good)) / differences.shape[0] < 0.7:
+		return None
+
+	# translate previous homography based on the flow result
+	translation = [numpy.median(differences[:, 0]), numpy.median(differences[:, 1])]
+	H_translation = numpy.array([[1, 0, -translation[1]], [0, 1, -translation[0]], [0,0,1]], dtype='float32')
+	return prev_homography.dot(H_translation)
+
--- visionprocessor/embeddings.py
+++ visionprocessor/embeddings.py
@@ -1,100 +1,152 @@
-# Import required libraries  
-import os  
+#! /usr/bin/python
+
+#from azure.ai.vision import VisionClient
+from azure.core.credentials import AzureKeyCredential
+from azure.core.rest import HttpRequest, HttpResponse
+from azure.core.exceptions import HttpResponseError
+from azure.identity import DefaultAzureCredential
+from azure.search.documents import SearchClient
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures, ImageAnalysisResult
+from tenacity import retry, stop_after_attempt, wait_fixed
+from pprint import pprint, pformat
+from dotenv import load_dotenv  
 import json  
 import requests
 import http.client, urllib.parse
-from tenacity import retry, stop_after_attempt, wait_fixed
-from dotenv import load_dotenv  
-from azure.core.credentials import AzureKeyCredential
-from azure.identity import DefaultAzureCredential
-from azure.search.documents import SearchClient  
-from azure.search.documents.indexes import SearchIndexClient  
-from azure.search.documents.models import (
-    RawVectorQuery,
-)
-from azure.search.documents.indexes.models import (  
- 
-    ExhaustiveKnnParameters,  
-    ExhaustiveKnnVectorSearchAlgorithmConfiguration,
-    HnswParameters,  
-    HnswVectorSearchAlgorithmConfiguration,
-    SimpleField,
-    SearchField,  
-    SearchFieldDataType,  
-    SearchIndex,  
-    VectorSearch,  
-    VectorSearchAlgorithmKind,  
-    VectorSearchProfile,  
-)
+import os
 
-from IPython.display import Image, display
-
-from sklearn.metrics.pairwise import cosine_similarity
-  
 load_dotenv()  
-service_endpoint = os.getenv("AZURE_SEARCH_SERVICE_ENDPOINT")  
+search_endpoint = os.getenv("AZURE_SEARCH_SERVICE_ENDPOINT")  
 index_name = os.getenv("AZURE_SEARCH_INDEX_NAME")
-api_version = os.getenv("AZURE_SEARCH_API_VERSION")
-key = os.getenv("AZURE_SEARCH_ADMIN_KEY")  
-aiVisionApiKey = os.getenv("AZURE_AI_VISION_API_KEY")  
-aiVisionRegion = os.getenv("AZURE_AI_VISION_REGION")
-aiVisionEndpoint = os.getenv("AZURE_AI_VISION_ENDPOINT")
+search_api_version = os.getenv("AZURE_SEARCH_API_VERSION")
+search_api_key = os.getenv("AZURE_SEARCH_ADMIN_KEY")  
+vision_api_key = os.getenv("AZURE_AI_VISION_API_KEY")
+vision_api_version = os.getenv("AZURE_AI_VISION_API_VERSION")
+vision_region = os.getenv("AZURE_AI_VISION_REGION")
+vision_endpoint =  os.getenv("AZURE_AI_VISION_ENDPOINT")
 credential = DefaultAzureCredential()
-FILE_PATH = os.getenv("FILE_PATH") or 'data/frames/main'  # Path to the images directory
+#search_credential = AzureKeyCredential(search_api_key)
+vision_credential = AzureKeyCredential(vision_api_key)
+
+# Initialize Azure clients
+#vision_client = VisionClient(endpoint=vision_endpoint, credential=AzureKeyCredential(vision_api_key))
+search_client = SearchClient(endpoint=search_endpoint, index_name=index_name, credential=AzureKeyCredential(search_api_key))
+analysis_client = ImageAnalysisClient(vision_endpoint, vision_credential)
+
+# Define SAS URL template
+sas_template = os.getenv("AZURE_SA_CONTAINER_SASURL")
+
+# Process images in batches of 10
+batch_size = 10
+initial_start = 2040
+total_images = 17853  # Adjust this as needed
 
 
-@retry(stop=stop_after_attempt(5), wait=wait_fixed(1))
-def get_image_vector(image_path, key, region):
-    headers = {
-        'Ocp-Apim-Subscription-Key': key,
-    }
-
-    params = urllib.parse.urlencode({
-        'model-version': '2023-04-15',
-    })
-
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(60))
+def vectorize_image(image_path, key, region):
     try:
-        if image_path.startswith(('http://', 'https://')):
-            headers['Content-Type'] = 'application/json'              
-            body = json.dumps({"url": image_path})
+        # API version and model version
+        api_version = "2024-02-01"
+        model_version = "2023-04-15"
+
+        # Construct the request URL
+        url = f"{vision_endpoint}/computervision/retrieval:vectorizeImage?api-version={api_version}&model-version={model_version}"
+
+        # Set headers
+        headers = {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": key
+        }
+
+        # Set the payload with the SAS URL
+        payload = {
+            "url": image_path
+        }
+
+        # Make the POST request
+        response = requests.post(url, headers=headers, json=payload)
+
+        # Check the response
+        if response.status_code == 200:
+            result = response.json()
+            # The vector is in the 'vector' field of the response
+            vector = result.get("vector")
+            # print("Vector embedding:", vector)
+            return vector
         else:
-            headers['Content-Type'] = 'application/octet-stream'
-            with open(image_path, "rb") as filehandler:
-                image_data = filehandler.read()
-                body = image_data
-
-        conn = http.client.HTTPSConnection(f'{region}.api.cognitive.microsoft.com', timeout=3)
-        conn.request("POST", "/computervision/retrieval:vectorizeImage?api-version=2023-04-01-preview&%s" % params, body, headers)
-        response = conn.getresponse()
-        data = json.load(response)
-        conn.close()
-
-        if response.status != 200:
-            raise Exception(f"Error processing image {image_path}: {data.get('message', '')}")
-
-        return data.get("vector")
+            print("Error:", response.status_code, response.text)
+            vector = [0.0] * 1024
+            raise Exception(f"Error vectorizing image {image_path[74:80]}")
 
     except (requests.exceptions.Timeout, http.client.HTTPException) as e:
-        print(f"Timeout/Error for {image_path}. Retrying...")
+        print(f"Timeout/Error for {image_path[74:80]}. Retrying...")
         raise
 
-image_embeddings = {}
-DIR_PATH = os.path.join(os.getcwd(), FILE_PATH)
-FILES = os.listdir(FILE_PATH)
-for file in FILES:
-    image_embeddings[file] = get_image_vector(os.path.join(DIR_PATH, file), 
-                                    aiVisionApiKey, aiVisionRegion)
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(60))
+def analyze_image(client, image_url):
+    try:
+        # Define all available visual features for analysis
+        features = [
+            VisualFeatures.CAPTION,
+            VisualFeatures.TAGS,
+            VisualFeatures.OBJECTS,
+            VisualFeatures.READ,
+            VisualFeatures.SMART_CROPS,
+            VisualFeatures.DENSE_CAPTIONS,
+            VisualFeatures.PEOPLE
+        ]
+        
+        # Analyze the image from the SAS URL
+        result = client.analyze_from_url(
+            image_url=image_url,
+            visual_features=features,
+            gender_neutral_caption=True        )
+        # Explicitly cast to ImageAnalysisResult (for clarity)
+        result: ImageAnalysisResult = result
+        if result is not None:
+            captions = []
+            captions += [ f"{result.caption.text}" if result.caption is not None else "No Caption"]
+            captions += [ f"{caption.text}" for caption in result.dense_captions.list if result.dense_captions is not None]
+            # Enhance result
+            result.description = ",".join(captions)
+            # print("Full ImageAnalysisResult object:")
+            # pprint(result.__dict__, depth=4, compact=False)
+            description =  pformat(result.__dict__, depth=4, compact=False)
+            return description
+    except HttpResponseError as e:
+        print(str(e))
+        raise
+    return "No description"
 
-counter = 0
-input_data =[]
-for file in FILES:
-    d= {}
-    d["id"] = str(counter)
-    d["description"] = file
-    d["image_vector"] = image_embeddings[file]
-    input_data.append(d)
-    counter += 1
 
-# Output embeddings to docVectors.json file
-with open("output/docVectors.json", "w") as f:
-    json.dump(input_data, f)
+for batch_start in range(initial_start, total_images + 1, batch_size):
+    documents = []
+
+    # Vectorize 100 images at a time
+    batch_end = min(batch_start + batch_size, total_images + 1)
+    for i in range(batch_start, batch_end):
+        file_name = f"{i:06}"
+        blob_url = sas_template.format(file=file_name)
+
+        try:
+            vector = vectorize_image(blob_url, vision_api_key, "eastus")
+            if vector is not None:
+               description = analyze_image(analysis_client, blob_url)
+               documents += [
+                  {"id": file_name, "description": description, "image_vector": vector}
+               ]
+        except Exception as e:
+            print(f"Error processing {file_name}.jpg: {e}")
+
+    # print(f"Vectorization complete for images {batch_start} to {min(batch_start + batch_size - 1, total_images)}")
+    # Upload batch to Azure AI Search
+    if len(documents) > 0:
+        # [pprint(document, depth=4, compact=False) for document in documents]
+        try:
+            search_client.upload_documents(documents=documents)
+            print(f"Uploaded {len(documents)} images {batch_start} to {batch_end} to {index_name}.")
+        except Exception as e:
+            print(f"Error {e} uploading {len(documents)} images {batch_start} to {batch_end} to {index_name}.")
+
+print(f"Vectorized images successfully added to {index_name}!")
