6d20b68f6931ae0e8cff676cd1c46c5170c7bbb2
Ravi Rajamani <ravibeta@hotmail.com>
2026-02-21 16:22:40 -0800

    Revert "analytics_wip_py"
    This reverts commit 0d32f71efe994515b921f2ae4900dd42f3766e04.

--- analytics/convert-id.bat
+++
@@ -1,1 +0,0 @@
-az search index update  --name index007  --service-name srch-vision-01  --resource-group rg-ctl-2  --fields name=id type=Edm.String key=true filterable=true retrievable=true
--- analytics/upd.bat
+++
@@ -1,1 +0,0 @@
-az search index update  --name index00   --service-name srch-vision-01   --resource-group rg-ctl-2   --set defaultSemanticConfiguration="mysemantic"
